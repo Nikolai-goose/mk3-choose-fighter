135033d91dd70a17ffa31cd3cca75cc706034779
Nikolay Gushchin <akors99@gmail.com>
2020-06-08 12:01:08 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Select your fighter</h1>
+        </div>
+    );
+}
+
+export default App;
